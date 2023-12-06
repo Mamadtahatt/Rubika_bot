45954590219156dc5de70b37fe934841cdcd1c08
Mohammad taha safari <108574739+Mamadtahatt@users.noreply.github.com>
2023-12-06 20:17:13 +0330

    Add files via upload

---
+++ app.py
@@ -0,0 +1,109 @@
+from rubpy import Client, Message, handlers, models
+from asyncio import run, create_task
+from youtube_dl import youtube
+import httpx
+import random
+import string
+
+httpx_client = httpx.AsyncClient()
+yt = youtube()
+music_id: dict = {}
+
+async def random_string() -> str:
+    return ''.join(random.choice(string.ascii_letters) for _ in range(7))
+
+async def download(url: str) -> bytes:
+	try:
+		response = await httpx_client.get(url)
+	except:
+		return None
+		
+	if response.status_code == 200:
+		return response.read()
+	else:
+		return None
+	
+async def SearchApi(text: str) -> list:
+	try:
+		request = await httpx_client.get(
+			url = f"https://pipedapi.kavin.rocks/search?q={text}&filter=music_songs"
+		)
+	except:
+		return None
+		
+	if request.status_code == 200:
+		result = request.json()
+		result_list = []
+		for item in result.get("items"):
+			item["url"] = "https://youtube.com" + item.get("url")
+			result_list.append(
+				{
+					"title": item.get("title"),
+					"url": item.get("url")
+				}
+			)
+		return result_list
+	else:
+		return None
+		
+async def private_handler(client: Client, message: Message):
+	if isinstance(message.raw_text, str):
+		object_guid: str = message.object_guid
+		message_id: str = message.message_id
+		text: str = message.raw_text
+		
+		if text.startswith("!"):
+			replace_text = text.replace("! ", "")
+			search_result = await SearchApi(replace_text)
+			if isinstance(search_result, list):
+				list_music = ["🔎 نتایج یافت شده برای جستجوی شما:"]
+				
+				for result in search_result:
+					_id = "#" + await random_string()
+					music_id[_id] = result.get("url")
+					list_music.append("نام: {}\nایدی دانلود: {}".format(result.get("title"), _id))
+				
+				list_music = "\n\n".join(list_music)
+				await message.reply(list_music)
+			else:
+				await message.reply("خطا!")
+		
+		elif text.startswith("#"):
+			if text in music_id.keys():
+				await message.reply("درحال انجام عملیات . . .")
+				result = await yt.getURL(music_id[text])
+				
+				if isinstance(result, dict):
+					key = result.get("links").get("mp3").get("mp3128").get("k")
+					vid_id = result.get("vid")
+					dl_link = await yt.getDownload(vid=vid_id, key=key)
+
+					if isinstance(dl_link, dict):
+						music_bytes = await download(dl_link.get("dlink"))
+						if isinstance(music_bytes, bytes):
+							await client.send_music(
+								object_guid = object_guid,
+								music = music_bytes,
+								file_name = dl_link.get("title") + ".mp3",
+								performer = "youtube music",
+								caption = dl_link.get("title"),
+								reply_to_message_id = message_id
+							)
+						else:
+							await message.reply("خطا در دانلود فایل!")
+					else:
+						await message.reply("خطا در دریافت اطلاعات!")
+				else:
+					await message.reply("خطا در دریافت اطلاعات!")
+			else:
+				await message.reply("موزیکی با این آیدی یافت نشد!")
+	
+async def main():
+	async with Client(session="rubpy") as client:
+		@client.on(handlers.MessageUpdates(models.is_private))
+		async def updates_user(message: Message):
+			create_task(private_handler(client, message))
+			
+		await client.run_until_disconnected()
+		
+run(main())
---
+++ youtube_dl.py
@@ -0,0 +1,70 @@
+import httpx
+from user_agent import generate_user_agent
+
+class youtube(object):
+	def __init__(self):
+		self.client = httpx.AsyncClient()
+		self.user_agent = generate_user_agent()
+	
+	def header(self):
+		headers = {
+			"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+			"Accept": "*/*",
+			"X-Requested-With": "XMLHttpRequest",
+			"User-Agent": self.user_agent,
+			"Referer": "https://yt1s.com/en627"
+		}
+		return headers
+		
+	async def getURL(self, link: str):
+		api_url = "https://yt1s.com/api/ajaxSearch/index"
+		try:
+			response = await self.client.post(
+				api_url,
+				headers=self.header(),
+				data={
+					"q": link,
+					"vt": "home"
+				}
+			)
+			if response.status_code == 200:
+				return response.json()
+			else:
+				return None
+		except:
+			return None
+	
+	async def getDownload(self, key: str, vid: str, save: bool = False):
+		api_url = "https://yt1s.com/api/ajaxConvert/convert"
+		try:
+			response = await self.client.post(
+				api_url,
+				headers=self.header(),
+				data={
+					"vid": vid,
+					"k": key
+				}
+			)
+			if response.status_code == 200:
+				if save:
+					download = await self.client.get(
+						url=response.json()["dlink"]
+					)
+					if download.status_code == 200:
+						content, name, ftype = (
+							download.content,
+							response.json()["title"],
+							response.json()["ftype"]
+						)
+						with open(name + "." + ftype, "wb") as file:
+							file.write(content)
+							file.close()
+						return True
+					else:
+						return False
+				else:
+					return response.json()
+			else:
+				return None
+		except:
+			return None
